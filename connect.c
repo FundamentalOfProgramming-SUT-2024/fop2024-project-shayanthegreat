686c649f5118394626b7e24311caef70c5f72b62
ShayanTheGreat <mr.sabzi6@gmail.com>
2025-02-02 22:04:38 +0330

    What you want TA?

--- connect.c
+++ connect.c
@@ -7,7 +7,7 @@
 #include "connect.h"
 
 bool is_new(char *user_name){
-    char folderName[5000] = "/home/crk/projeh/user/";
+    char folderName[5000] = "user/";
     strcat(folderName, user_name);
     DIR *dir = opendir(folderName);
     if (dir) {
@@ -17,41 +17,41 @@ bool is_new(char *user_name){
     return 1;
 }
 void Create_user(Userinfo* userinfo){
-    char filename[5000] = "/home/crk/projeh/user/";
+    char filename[5000] = "user/";
     strcat(filename, userinfo->user_name);
     mkdir(filename, 0755);
     strcat(filename, "/email.txt");
     FILE *file = fopen(filename, "w");
     fprintf(file, "%s", userinfo->user_email);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/pass.txt");
     file = fopen(filename, "w");
     fprintf(file, "%s", userinfo->user_pass);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/exp.txt");
     file = fopen(filename, "w");
     userinfo->user_exp = time(NULL);
     fprintf(file, "%d", userinfo->user_exp);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/gold.txt");
     file = fopen(filename, "w");
     userinfo->user_gold = 0;
     fprintf(file, "%d", userinfo->user_gold);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/point.txt");
     file = fopen(filename, "w");
     userinfo->user_point = 0;
     fprintf(file, "%d", userinfo->user_point);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/game.txt");
     file = fopen(filename, "w");
@@ -60,38 +60,38 @@ void Create_user(Userinfo* userinfo){
     fclose(file);
 }
 void Fetch_user(char* user_name){
-    char filename[5000] = "/home/crk/projeh/user/";
+    char filename[5000] = "user/";
     strcat(filename, user_name);
     strcat(filename, "/pass.txt");
     FILE *file = fopen(filename, "r");
     fgets(userinfo->user_pass, 50, file);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, user_name);
     strcat(filename, "/email.txt");
     file = fopen(filename, "r");
     fgets(userinfo->user_email, 50, file);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/exp.txt");
     file = fopen(filename, "r");
     fscanf(file, "%d", &userinfo->user_exp);
     fclose(file);
     ///
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/gold.txt");
     file = fopen(filename, "r");
     fscanf(file, "%d", &userinfo->user_gold);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/point.txt");
     file = fopen(filename, "r");
     fscanf(file, "%d", &userinfo->user_point);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/game.txt");
     file = fopen(filename, "r");
@@ -104,19 +104,19 @@ void Update_user(Userinfo* userinfo){
     userinfo->user_point += User->point * 2;
     userinfo->user_point += User->gold;
     userinfo->user_gold += User->gold;
-    char filename[5000] = "/home/crk/projeh/user/";
+    char filename[5000] = "user/";
     strcat(filename, userinfo->user_name);
     strcat(filename, "/gold.txt");
     FILE *file = fopen(filename, "w");
     fprintf(file, "%d", userinfo->user_gold);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/point.txt");
     file = fopen(filename, "w");
     fprintf(file, "%d", userinfo->user_point);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, userinfo->user_name);
     strcat(filename, "/game.txt");
     file = fopen(filename, "w");
@@ -128,26 +128,26 @@ void Update_user(Userinfo* userinfo){
 
 
 Userinfo Fetch_user2(char* user_name){
-    char filename[5000] = "/home/crk/projeh/user/";
+    char filename[5000] = "user/";
     Userinfo userx;
     strcat(filename, user_name);
     strcat(filename, "/exp.txt");
     FILE *file = fopen(filename, "r");
     fscanf(file, "%d", &userx.user_exp);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, user_name);
     strcat(filename, "/gold.txt");
     file = fopen(filename, "r");
     fscanf(file, "%d", &userx.user_gold);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, user_name);
     strcat(filename, "/point.txt");
     file = fopen(filename, "r");
     fscanf(file, "%d", &userx.user_point);
     fclose(file);
-    strcpy(filename, "/home/crk/projeh/user/");
+    strcpy(filename, "user/");
     strcat(filename, user_name);
     strcat(filename, "/game.txt");
     file = fopen(filename, "r");
